80a38f1a9dcff445780de2e96ce3533938b7ff20
Andile Zola <aczola@gmail.com>
2018-07-10 21:29:12 +0200

    Add project files.

---
+++ MvcCustomAuthourization/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using MvcCustomAuthourization.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcCustomAuthourization.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize(Roles="Admin")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User model,string returnUrl)
+        {
+            CustomRolesConceptEntities db = new CustomRolesConceptEntities();
+            var dataItem = db.Users.Where(x => x.EmailAddress == model.EmailAddress && x.Password == x.Password).First();
+            if(dataItem != null)
+            {
+                FormsAuthentication.SetAuthCookie(dataItem.EmailAddress,false);
+                if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/") && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("","Invalid user/pass");
+                return View();
+            }
+        }
+        [Authorize]
+        [AllowAnonymous]
+        public ActionResult Signout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login","Home");
+        }
+    }
+}
---
+++ MvcCustomAuthourization/MyRoleProvider/SiteRole.cs
@@ -0,0 +1,76 @@
+﻿using MvcCustomAuthourization.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace MvcCustomAuthourization.MyRoleProvider
+{
+    public class SiteRole : RoleProvider
+    {
+        public override string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            CustomRolesConceptEntities db = new CustomRolesConceptEntities();
+            string data = db.Users.Where(x => x.EmailAddress == username).FirstOrDefault().UserTypeId.ToString();
+            //List<string> Roles = new List<string>();
+            //foreach(string dt in data)
+            //{
+            //    if(dt == "1")
+            //    {
+            //        Roles.Add("Admin");
+            //    }
+            //}
+            string Role = data == "1" ? "Admin" : "General";
+            string[] result = { Role };
+            return result;
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
